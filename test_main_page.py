16e310663d880224f64d065d116c9a00566b863e
q7w7e7r <q7w7e7r@gmail.com>
2021-03-16 21:20:30 +0600

    add test_guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -11,3 +11,9 @@ def test_guest_can_go_to_login_page(browser):
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
