ca329aaf574b027fd8fc2ec193660842fbb95397
q7w7e7r <q7w7e7r@gmail.com>
2021-03-19 21:12:01 +0600

    added ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM=(By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    CART = (By.CSS_SELECTOR,".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    NAME_PRODUCT_ADDET_TO_CART = (By.CSS_SELECTOR,"#messages > div:nth-child(1) > div > strong")
