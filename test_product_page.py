2d85aaea5de1b8da1827d9edd13226d87a8b93fc
q7w7e7r <q7w7e7r@gmail.com>
2021-03-20 18:31:41 +0600

    Добавил тесты с параметризацей и моетил один xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,33 @@
+import pytest
 from .pages.product_page import ProductPage
 
-def test_add_product_to_cart(browser):
+def test_guest_can_add_product_to_basket(browser):
     link="http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     assert "promo=newYear" in link, "the link does not contain promo=newYear"
     page = ProductPage(browser,link)
+    page.open()
+    page.add_product_to_cart()
+
+def test_guest_can_add_product_to_basket_2(browser):
+    link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    assert "promo=newYear" in link, "the link does not contain promo=newYear"
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_product_to_cart()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket_3(browser, link):
+    # link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    assert "promo=offer" in link, f"{link} the link does not contain promo=newYear"
+    page = ProductPage(browser,link)
+    page.open()
     page.add_product_to_cart()
