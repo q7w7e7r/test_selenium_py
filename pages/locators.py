a40978dd3ff58efc22aef29f03e40e29c169195d
q7w7e7r <q7w7e7r@gmail.com>
2021-03-20 18:22:42 +0600

    added locator PRODUCT_DESCRIPTION

--- pages/locators.py
+++ pages/locators.py
@@ -4,11 +4,14 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
-    LOGIN_FORM=(By.CSS_SELECTOR, "#login_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
 class ProductPageLocators():
-    CART = (By.CSS_SELECTOR,".btn-add-to-basket")
+    CART = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
-    NAME_PRODUCT_ADDET_TO_CART = (By.CSS_SELECTOR,"#messages > div:nth-child(1) > div > strong")
+    NAME_PRODUCT_ADDET_TO_CART = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_DESCRIPTION = (By.CSS_SELECTOR, "#product_description + p")
