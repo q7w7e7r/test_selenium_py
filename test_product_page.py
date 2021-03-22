28c4edd52e6152e2c82b072d4e086604fb057bd5
q7w7e7r <q7w7e7r@gmail.com>
2021-03-22 15:14:42 +0600

    добавил тесты для отображения ссылки логина и перехода по ссылке логина

--- test_product_page.py
+++ test_product_page.py
@@ -22,3 +22,15 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_added_product_name_with_alert()
     page.check_price_pr_info_with_alert()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
