794583e0671cc4c9803c5bc7b62a5b202e277918
q7w7e7r <q7w7e7r@gmail.com>
2021-03-22 22:36:43 +0600

    move def test_guest_can_go_to_login_page, test_guest_should_see_login_link in created class TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,37 +1,46 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage.should_be_login_page(browser,browser.current_url)
-    login_page.should_be_login_page()
 
-def go_to_login_page(browser):
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
+def go_to_login_page(self, browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 def test_change_language_on_de(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.change_language_on_de()
-    assert  page.check_current_language("Deutsch")
+    assert page.check_current_language("Deutsch")
+
 
 def test_find_oscar(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     assert page.find("Oscar T-shirt"), "Oscar T-shirt not found"
+
 
 # Гость открывает главную страницу
 # Переходит в корзину по кнопке в шапке сайта
