cf9d5a12feb20c3781df797ec7616df87f46cbcf
q7w7e7r <q7w7e7r@gmail.com>
2021-03-22 13:02:29 +0600

    перемещение локаторов ссылки на логин в BasePageLocator

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    pass
 
 
 class LoginPageLocators():
@@ -17,3 +17,8 @@ class ProductPageLocators():
     DESCRIPTION = (By.CSS_SELECTOR, "#product_description + p")
     PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     PRICE_IN_ALERT = (By.CSS_SELECTOR, "#messages > .alert-info > div > p:nth-child(1) > strong")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
