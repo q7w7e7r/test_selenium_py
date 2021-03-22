89686d7433b512eb065dc7b01656c5215ef0bcc2
q7w7e7r <q7w7e7r@gmail.com>
2021-03-22 22:34:29 +0600

    fix bug in def find

--- pages/base_page.py
+++ pages/base_page.py
@@ -54,7 +54,7 @@ class BasePage():
     def find(self,something):
         self.browser.find_element(*BasePageLocators.FIND_TEXT).send_keys(f"{something}")
         self.browser.find_element(*BasePageLocators.BTN_FIND).click()
-        self.is_element_present(*BasePageLocators.OSCAR)
+        return self.is_element_present(*BasePageLocators.OSCAR)
 
     def open_basket(self):
         self.browser.find_element(*BasePageLocators.BTN_VIEW_BASKET).click()
