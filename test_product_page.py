a58fe41dbad709183f09011037fced325bd0a808
q7w7e7r <q7w7e7r@gmail.com>
2021-03-23 16:26:13 +0600

    marks defs     test_user_can_add_product_to_basket     test_guest_can_add_product_to_basket     test_guest_cant_see_product_in_basket_opened_from_product_page     test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,9 @@
 import pytest, time
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -30,11 +31,26 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+# Открываем страницу товара
+# Переходим в корзину по кнопке в шапке сайта
+# Проверяем, что в корзине нет товаров
+# Проверяем, что есть текст на английском о том, что корзина пуста
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = BasketPage(browser, browser.current_url)
+    cart_page.cart_should_be_empty()
+    cart_page.cart_should_have_empty_message()
 
 class TestUserAddToBasketFromProductPage():
 
@@ -61,6 +77,7 @@ class TestUserAddToBasketFromProductPage():
     # Получаем код
     # Проверяем название товара
     # Проверяем цену товара
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
