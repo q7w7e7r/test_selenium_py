67627e0d3a2103ea414cf8700003cdd5868ed302
q7w7e7r <q7w7e7r@gmail.com>
2021-03-23 13:32:06 +0600

    add locators class LoginPageLocators     EMAIL_INPUT     PASSWORD_INPUT_1     PASSWORD_INPUT_2     BUTTON_REGISTRATION class BasePageLocators():     USER_ICON
    Change locators class ProductPageLocators():
        SUCCESS_MESSAGE
        PRICE_IN_ALERT

--- pages/locators.py
+++ pages/locators.py
@@ -8,15 +8,19 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_INPUT = (By.CSS_SELECTOR,'#id_registration-email')
+    PASSWORD_INPUT_1 = (By.CSS_SELECTOR,'#id_registration-password1')
+    PASSWORD_INPUT_2 = (By.CSS_SELECTOR,'#id_registration-password2')
+    BUTTON_REGISTRATION = (By.CSS_SELECTOR,'button[name="registration_submit"]')
 
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
-    NAME_IN_ALERT = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner")
     DESCRIPTION = (By.CSS_SELECTOR, "#product_description + p")
     PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    PRICE_IN_ALERT = (By.CSS_SELECTOR, "#messages > .alert-info > div > p:nth-child(1) > strong")
+    PRICE_IN_ALERT = (By.CSS_SELECTOR, ".alertinner p strong")
 
 
 class BasePageLocators():
@@ -29,6 +33,7 @@ class BasePageLocators():
     FIND_TEXT = (By.CSS_SELECTOR, '#id_q')
     OSCAR = (By.CSS_SELECTOR, "[title = 'Oscar T-shirt']")
     BTN_VIEW_BASKET = (By.CSS_SELECTOR,".basket-mini a.btn")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     TITLE =(By.CSS_SELECTOR, '#content_inner > .basket-title')
