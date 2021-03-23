9b148b390e422ea169c538b4c2957bdd5623b148
q7w7e7r <q7w7e7r@gmail.com>
2021-03-23 16:24:37 +0600

    rename defs on cart_should_be_empty, cart_should_have_empty_message

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,9 +5,9 @@ from .locators import BasketPageLocators
 import math
 
 class BasketPage(BasePage):
-    def check_empty_product(self):
+    def cart_should_be_empty(self):
         assert not self.is_element_present(*BasketPageLocators.TITLE), \
             "There are products in the shopping cart"
-    def check_message_empty(self):
+    def cart_should_have_empty_message(self):
         assert  self.is_element_present(*BasketPageLocators.EMPTY_MESSAGE), \
             "Not found message 'empty basket'"
